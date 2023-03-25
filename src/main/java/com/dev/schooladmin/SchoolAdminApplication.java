cec9c5ef930610021862873c0c31e27c00053ed8
fauchard <ifauchard@163.com>
2023-03-25 16:17:08 +0800

    first schoolproject init

---
+++ src/main/java/com/dev/schooladmin/SchoolAdminApplication.java
@@ -0,0 +1,13 @@
+package com.dev.schooladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/schooladmin/SchoolAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.schooladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
